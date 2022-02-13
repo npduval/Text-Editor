3fe12dcf72d09429ff2f9d48ce3ed4719b614870
Nick Duval <nicholaspduval74@gmail.com>
2022-02-13 11:44:46 -0500

    updated databse.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,40 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT from the database');
+
+  const Db = await openDB('jate', 1);
+
+  const tx = Db.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.put({ id: id, content: content });
+
+  const result = await request;
+
+  console.log('data saved to the database', result);
+ 
+  return result;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => { 
+  
+console.log('GET from the database');
+
+const Db = await openDB('jate', 1);
+
+const tx = Db.transaction('jate', 'readonly');
+
+const store = tx.objectStore('jate');
+
+const request = store.getAll();
+
+const result = await request;
+console.log('result.value', result);
+return result;
+};
 
 initdb();
